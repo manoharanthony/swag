c038fdc450027165b00ce0b8a28526eceb04d4a6
manoharanthony <manohar.anthony@wohlig.in>
2019-10-23 18:31:09 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,372 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <!-- Breadcrumb start -->
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Home</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Clothing</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Men's Clothing</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#">Shirts</a>
+          </li>
+        </ol>
+      </nav>
+      <!-- Breadcrumb end -->
+
+      <div class="main">
+        <div class="row">
+          <!-- Shirt image start -->
+          <div class="col-md-4">
+            <div class="swag-shirt text-left card shadow">
+              <img src="../assets/img/1.jpeg" alt class="swag-shirt-img" />
+            </div>
+
+            <div class="slider">
+              <div class="row">
+                <div class="col-md-3">
+                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" />
+                </div>
+                <div class="col-md-3">
+                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" />
+                </div>
+                <div class="col-md-3">
+                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" />
+                </div>
+                <div class="col-md-3">
+                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" />
+                </div>
+              </div>
+            </div>
+
+            <!-- Slider container -->
+            <!-- <div class="swiper-container">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" /></div>
+                <div class="swiper-slide"><img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" /></div>
+                <div class="swiper-slide"><img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" /></div>
+                <div class="swiper-slide"><img src="../assets/img/1.jpeg" alt class="swag-shirt-var1-img" /></div>
+              </div>
+            </div>-->
+            <!-- Slider container end -->
+          </div>
+
+          <!-- Shirt image end -->
+
+          <div class="col-md-8">
+            <div class="bg-light p15">
+              <div class="section1">
+                <!-- Shirt Name and Prize start -->
+                <div class="shirt-title fjalla-one f26 text-left border-bottom-gray">
+                  <div class="text-uppercase">Lines full sleeve shirt with rollup</div>
+                  <div class="shirt-price mb15">
+                    <span class="rupee-symbol">&#8377;</span>
+                    2,899
+                  </div>
+                </div>
+                <!-- Shirt Name and Prize end -->
+              </div>
+
+              <div class="section2 border-bottom-gray mt10">
+                <div class="mb15">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <!-- Sizes start -->
+                      <div class="sizes">
+                        <div
+                          class="size-text f20 opensans-semibold text-uppercase text-left mb10"
+                        >Size</div>
+                        <ul class="text-left f22 list-inline list-unstyled text-uppercase">
+                          <li class="list-inline-item">
+                            <a href="#" class="size-list border bg-light">s</a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a href="#" class="size-list border m-1 bg-light">m</a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a href="#" class="size-list border m-1 bg-light">l</a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a href="#" class="size-list border m-1 bg-light">xl</a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a href="#" class="size-list border bg-light">xxl</a>
+                          </li>
+                        </ul>
+                        <div class="size-chart text-left f12">
+                          <a href="#" class="text-muted">Size chart</a>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Sizes end -->
+
+                    <!-- Select color start -->
+
+                    <div class="col-md-6">
+                      <div class="select-color">
+                        <div class="text-uppercase f20 opensans-semibold text-left">Select Color</div>
+                        <div class="shirt-color text-left">
+                          <ul class="list-inline list-unstyled">
+                            <li class="list-inline-item">
+                              <a href="#">
+                                <div class="shirt-color-img">
+                                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-color-img" />
+                                </div>
+                              </a>
+                            </li>
+                            <li class="list-inline-item">
+                              <a href="#">
+                                <div class="shirt-color-img">
+                                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-color-img" />
+                                </div>
+                              </a>
+                            </li>
+                            <li class="list-inline-item">
+                              <a href="#">
+                                <div class="shirt-color-img">
+                                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-color-img" />
+                                </div>
+                              </a>
+                            </li>
+                            <li class="list-inline-item">
+                              <a href="#">
+                                <div class="shirt-color-img">
+                                  <img src="../assets/img/1.jpeg" alt class="swag-shirt-color-img" />
+                                </div>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- Select color end -->
+                  </div>
+                </div>
+              </div>
+
+              <div class="section3 border-bottom-gray">
+                <div class="f20 opensans-semibold text-uppercase text-left mt10">Special Offers</div>
+                <ul class="list-unstyled text-left mb10">
+                  <li>Instant 5% discount on all prepaid orders.</li>
+                  <li
+                    class="text-muted"
+                  >Offer applicable over current promotions and discounts T&amp;C.</li>
+                </ul>
+              </div>
+
+              <div class="section4 border-bottom-gray">
+                <div class="f20 opensans-semibold text-uppercase text-left mt10">Description</div>
+                <ul class="list-unstyled mb10">
+                  <li
+                    class="text-left text-muted"
+                  >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente temporibus sed quis fugiat fuga non modi. Corporis, non adipisci!</li>
+                </ul>
+              </div>
+
+              <div class="section5 border-bottom-gray">
+                <div class="f20 opensans-semibold text-uppercase text-left mt10">Specification</div>
+                <div class="mb10">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="specification">
+                        <div class="row">
+                          <div class="col-md-5 col-sm-5 col-xs-5 text-left">
+                            <div class="specification">Brand</div>
+                          </div>
+                          <div class="col-md-7 col-sm-7 col-xs-7 text-left">
+                            <div class="specification-text text-muted">MaroSwag</div>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-md-5 col-sm-5 col-xs-5 text-left">
+                            <div class="specification">Collection</div>
+                          </div>
+                          <div class="col-md-7 col-sm-7 col-xs-7 text-left">
+                            <div class="specification-text text-muted">Amten Prints</div>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-md-5 col-sm-5 col-xs-5 text-left">
+                            <div class="specification">Sleeve</div>
+                          </div>
+                          <div class="col-md-7 col-sm-7 col-xs-7 text-left">
+                            <div class="specification-text text-muted">Half Sleeve</div>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-md-5 col-sm-5 col-xs-5 text-left">
+                            <div class="specification">Fabric</div>
+                          </div>
+                          <div class="col-md-7 col-sm-7 col-xs-7 text-left">
+                            <div class="specification-text text-muted">56% Cotton, 44% Tensel</div>
+                          </div>
+                        </div>
+
+                        <div class="row mb10">
+                          <div class="col-md-5 col-sm-5 col-xs-5 text-left">
+                            <div class="specification">Base color</div>
+                          </div>
+                          <div class="col-md-7 col-sm-7 col-xs-7 text-left">
+                            <div class="specification text-muted">White</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="wash-care opensans-semibold text-uppercase text-left">Wash care</div>
+                      <div
+                        class="text-left text-muted"
+                      >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe deleniti porro ut dolorum sint ab veritatis aspernatur earum quod nostrum.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="section6">
+                <div class="mt10 mb10 text-left">
+                  <!-- <div class="row mb20">
+
+                  <div class="col-md-4">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <label class="form-check-label text-muted f14">Add to compare</label>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                    <a href="#" class="offers text-muted f14">Offers?</a>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="text-muted f14">Buy 1 Get 1 Free</div>
+                  </div>
+                  </div>-->
+
+                  <ul class="list-inline mb20">
+                    <li class="list-inline-item">
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <label class="form-check-label text-muted f14">Add to compare</label>
+                      </div>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="#" class="offers text-muted f14">Offers?</a>
+                    </li>
+                    <li class="list-inline-item text-muted f14">Buy 1 Get 1 Free</li>
+                  </ul>
+
+                  <button
+                    type="submit"
+                    class="btn text-uppercase text-white f22 cart-btn mr-4 mb15"
+                  >Add to cart</button>
+                  <button
+                    type="submit"
+                    class="btn text-uppercase text-white f22 wishlist-btn mb15"
+                  >Add to Wishlist</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "home",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/scss/main";
+
+.breadcrumb {
+  padding-left: 0px;
+  background-color: transparent;
+  color: black;
+
+  a {
+    color: black;
+  }
+}
+
+.breadcrumb-item + .breadcrumb-item::before {
+  content: ">";
+  color: black;
+}
+
+.main {
+  .swag-shirt {
+    .swag-shirt-img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .swag-shirt-var1-img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+.section1 {
+  .shirt-price {
+    .rupee-symbol {
+      margin-top: -3px;
+    }
+  }
+}
+
+.section2 {
+  .sizes {
+    .size-list {
+      padding: 1px 10px;
+      color: black;
+
+      &:hover {
+        background-color: $gray-dark-color !important;
+      }
+    }
+
+    .size-chart {
+      text-decoration: underline;
+    }
+  }
+
+  .shirt-color-img {
+    width: 64px;
+    height: 60px;
+    .swag-shirt-color-img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.section6 {
+  .offers {
+    text-decoration: underline;
+  }
+  .cart-btn,
+  .wishlist-btn {
+    background: $gray-dark-color;
+    padding: 2px 30px;
+    border-radius: 0px;
+  }
+}
+</style>
